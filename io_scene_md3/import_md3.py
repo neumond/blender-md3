c0f2ab35abbbff5ddaeb753ffaee5ab4fc184ed2
neumond <knifeslaughter@gmail.com>
2018-02-14 08:16:24 +0300

    Wipe license headers from source code

--- io_scene_md3/import_md3.py
+++ io_scene_md3/import_md3.py
@@ -1,17 +1,3 @@
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
 # TODO: different normals for shape keys
 # TODO: merge vertices near sharp edges (there is a disconnected surface now)
 # TODO: use_smooth=False for flat faces (all vertex normal equal)
