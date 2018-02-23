77a7da24124c1df66eb224bd1bbced233d79f6f2
neumond <knifeslaughter@gmail.com>
2018-02-23 21:55:40 +0300

    Fix travis

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 export BLENDER_USER_SCRIPTS=.
 export PYTHONPATH=.
 PARAMS=$(python -c 'from sys import argv; print(repr(argv[1:]))' $@)
-blender --factory-startup -noaudio --background --python-expr "import pytest; pytest.main($PARAMS)"
+blender --factory-startup -noaudio --background --python-exit-code 1 --python-expr "import pytest; pytest.main($PARAMS)"
